8d7693e44f92706313fed5f34cb0d68d8a7306b0
Mahalakshmi <mahait07@gmail.com>
2022-02-15 11:29:36 -0700

    code changed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 const theme = document.querySelector(".container");
 const form  = document.getElementById("form");
 const responseElement = document.querySelector('.response');
-
+//console.log(name.value);
 
 const selectTheme = function (event) {
   event.preventDefault();
   
   const season = form.season.value;
+ 
+  //console.log(season);
 
   if (season == "select"){
     event.preventDefault();
@@ -17,22 +19,22 @@ const selectTheme = function (event) {
   if (season === "summer") {
     theme.style.backgroundImage = "url(./images/summer.jpg)";
     theme.style.backgroundSize = "cover";
-    responseElement.textContent = "Summer is Vacation time";
+    responseElement.textContent =  `Your favourite season is ${season}`;
   } 
   else if (season === "spring") {
     theme.style.backgroundImage = "url(./images/spring.jpg)";
     theme.style.backgroundSize = "cover";
-    responseElement.textContent = "Spring is a time when flowers bloom and trees begin to grow and reproduce."
+    responseElement.textContent = `Your favourite season is ${season}`;
   } 
   else if (season === "fall") {
     theme.style.backgroundImage = "url(./images/fall.jpg)";
     theme.style.backgroundSize = "cover";
-    responseElement.textContent =" The tree leaves changing color, the temperature grows colder"
+    responseElement.textContent =`Your favourite season is ${season}`;
   } 
   else if(season === "winter") {
     theme.style.backgroundImage = "url(./images/winter.jpg)";
     theme.style.backgroundSize = "cover";    
-    responseElement.textContent ="Snow and frost, the trees have no leaves ";
+    responseElement.textContent =`Your favourite season is ${season}`;
   } 
   
 }
